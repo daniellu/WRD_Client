29dabe275e6d981753dca0bbaae36fa3b58e0e9b
Daniel <luzhanxun.daniel@gmail.com>
2017-06-07 15:19:50 -0700

    add more example

--- R/Test.R
+++ R/Test.R
@@ -4,11 +4,17 @@ library(httr)
 wrd_url <- "http://localhost:50772"
 authenticate_wrd(wrd_url, "Wellgreen", "Welcome123!")
 
-list_data <- get_guideline_list(wrd_url)
-detail_data <- get_guideline_detail_list(wrd_url)
+guideline_list_data <- get_guideline_list(wrd_url)
 
+df1 <- Reduce(rbind, guideline_list_data)
+
+
+guideline_detail_data <- get_guideline_detail_list(wrd_url)
+df2 <- Reduce(rbind, guideline_detail_data)
 
 location_list_data <- get_location_list(wrd_url)
+df3 <- Reduce(rbind, location_list_data)
+
 location_detail_date <- get_location_detail(wrd_url, "AdC-0.1")
 
 report_data <- get_report_data(wrd_url, 
